298e5ae50a3aa9ef424556972e08e7465ab5471d
Aditya Kishore <adityakishoe0698@gmail.com>
2026-07-28 00:44:45 +0530

    add get_db method to database.py and change database name to wallet_db

--- app/core/database.py
+++ app/core/database.py
@@ -1,8 +1,15 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-db_url = "postgresql+psycopg://postgres:postgres@localhost:5432/db_name"
+db_url = "postgresql+psycopg://postgres:postgres@localhost:5432/wallet_db"
 
 engine = create_engine(db_url)
 
-session = sessionmaker(bind=engine, autoflush=False, autocommit = False)
+SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit = False)
+
+def get_db():
+    session = SessionLocal()
+    try:
+        yield session
+    finally:
+        session.close()
